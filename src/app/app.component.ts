5d7a9cd0520d242c22d46bd3ece8c4eb78b523f1
Laszlo Nagy <nagyll92@gmail.com>
2020-02-29 08:35:47 +0200

    Start matereial (will be overritten)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,15 @@ export class AppComponent {
   title = 'learn-proj-ng';
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('keyboard-arrow-up', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/keyboard_arrow_up.svg'));
-    iconRegistry.addSvgIcon('keyboard-arrow-down', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/keyboard_arrow_down.svg'));
-    iconRegistry.addSvgIcon('build', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/build.svg'));
+    iconRegistry.addSvgIcon('keyboard-arrow-up', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/keyboard_arrow_up.svg'));
+    iconRegistry.addSvgIcon('keyboard-arrow-down', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/keyboard_arrow_down.svg'));
+    iconRegistry.addSvgIcon('build', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/build.svg'));
+    iconRegistry.addSvgIcon('save', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/save.svg'));
+    iconRegistry.addSvgIcon('delete', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/delete.svg'));
+    iconRegistry.addSvgIcon('cancel', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/cancel.svg'));
+    iconRegistry.addSvgIcon('add', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/add.svg'));
+    iconRegistry.addSvgIcon('add-o', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/add-o.svg'));
+    iconRegistry.addSvgIcon('edit', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/app_icons/edit.svg'));
   }
 
 }
